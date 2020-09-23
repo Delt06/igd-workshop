bd77e30bca2f171faf56de8ecfb099bd929a1365
Vladislav Kantaev <vvkantaev@yandex.ru>
2020-09-23 23:40:31 +0300

    Initial commit

---
+++ Assets/Scripts/ForceModes.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public sealed class ForceModes : MonoBehaviour
+{
+	[SerializeField] private ForceMode _forceMode = default;
+	[SerializeField] private Vector3 _value = default;
+
+	private void FixedUpdate()
+	{
+		switch (_forceMode)
+		{
+			case ForceMode.Force: // default force mode
+				_rigidbody.AddForce(_value);
+				break;
+			case ForceMode.Acceleration:
+				_rigidbody.AddForce(_value, ForceMode.Acceleration);
+				break;
+		}
+	}
+
+	private void Start()
+	{
+		switch (_forceMode)
+		{
+			case ForceMode.Impulse:
+				_rigidbody.AddForce(_value, ForceMode.Impulse);
+				break;
+			case ForceMode.VelocityChange:
+				_rigidbody.AddForce(_value, ForceMode.VelocityChange);
+				break;
+		}
+	}
+
+	private void Awake()
+	{
+		_rigidbody = GetComponent<Rigidbody>();
+	}
+
+	private Rigidbody _rigidbody;
+}
---
+++ Assets/Scripts/OnTriggerDisplayName.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public sealed class OnTriggerDisplayName : MonoBehaviour
+{
+	private void OnTriggerEnter(Collider other)
+	{
+		Debug.Log(other);
+	}
+}
