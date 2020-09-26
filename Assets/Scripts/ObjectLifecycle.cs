841a5bc64c962d2cf997a02fe9164e639ca09428
Vladislav Kantaev <vvkantaev@yandex.ru>
2020-09-26 19:46:27 +0300

    Add object lifecyle example

---
+++ Assets/Scripts/ObjectLifecycle.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class ObjectLifecycle : MonoBehaviour
+{
+	private void OnCollisionEnter(Collision other)
+	{
+		//Destroy(this); // destroy this component only
+		Destroy(gameObject); // destroy game object completely
+	}
+}
