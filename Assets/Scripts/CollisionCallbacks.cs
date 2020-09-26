587d2127458229aad83e50ff5ee4efd7f1adfa3c
Vladislav Kantaev <vvkantaev@yandex.ru>
2020-09-26 19:56:18 +0300

    Add collision trigger callbacks

---
+++ Assets/Scripts/CollisionCallbacks.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+public class CollisionCallbacks : MonoBehaviour
+{
+	private void OnCollisionEnter(Collision other)
+	{
+		Debug.Log("Contact began");
+		Debug.Log("Collision impulse" + other.impulse);
+		Debug.Log("Contacted with " + other.gameObject);
+	}
+
+	private void OnCollisionStay(Collision other)
+	{
+		Debug.Log("Contact continues");
+		// pitfall: will not be called if objects sleep
+	}
+
+	private void OnCollisionExit(Collision other)
+	{
+		Debug.Log("Contact ended");
+	}
+}
---
+++ Assets/Scripts/TriggerCallbacks.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+public class TriggerCallbacks : MonoBehaviour
+{
+	private void OnTriggerEnter(Collider other)
+	{
+		Debug.Log(other + " entered the trigger");
+	}
+
+	private void OnTriggerStay(Collider other)
+	{
+		Debug.Log(other + " is still inside the trigger");
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		Debug.Log(other + " exited the trigger");
+	}
+}
