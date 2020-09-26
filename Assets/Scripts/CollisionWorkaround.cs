7acef00024dc64db60304d8c5f42cee5414ff91a
Vladislav Kantaev <vvkantaev@yandex.ru>
2020-09-26 20:01:07 +0300

    Add collision workaround

---
+++ Assets/Scripts/CollisionWorkaround.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionWorkaround : MonoBehaviour
+{
+	private void OnCollisionEnter(Collision other)
+	{
+		_contacts.Add(other.collider);
+	}
+
+	private void FixedUpdate()
+	{
+		foreach (var contact in _contacts)
+		{
+			Debug.Log("Do work with " + contact);
+		}
+	}
+
+	private void OnCollisionExit(Collision other)
+	{
+		_contacts.Remove(other.collider);
+	}
+
+	private HashSet<Collider> _contacts = new HashSet<Collider>();
+}
